d4408304557c5f60e5782ba0d13af0c21addb8ad
niceyuanze <niceyuanze@gmail.com>
2020-11-11 20:46:50 +0800

    更新todo

--- src/main/java/datastructure/Category.java
+++ src/main/java/datastructure/Category.java
@@ -3,4 +3,5 @@ package datastructure;
 public class Category {
 //    https://github.com/wangzheng0822/algo/blob/master/java/07_linkedlist/LinkedListAlgo.java
 //    todo ，浏览器前进后退双栈
+//    阻塞队列，并发安全队列
 }
