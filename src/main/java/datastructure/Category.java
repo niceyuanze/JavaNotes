c9eed4d054e6861c29027695b5f82e0393cd372e
niceyuanze <niceyuanze@gmail.com>
2020-11-13 17:10:43 +0800

    update category in data structure

--- src/main/java/datastructure/Category.java
+++ src/main/java/datastructure/Category.java
@@ -1,7 +1,13 @@
 package datastructure;
 
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Category {
+
 //    https://github.com/wangzheng0822/algo/blob/master/java/07_linkedlist/LinkedListAlgo.java
-//    todo ，浏览器前进后退双栈
+//    todo ，浏览器前进后退双栈,四则混合预算
 //    阻塞队列，并发安全队列
+//    递归，检查环的存在，两步走一步走，电影院求当前是第几排
+
 }
