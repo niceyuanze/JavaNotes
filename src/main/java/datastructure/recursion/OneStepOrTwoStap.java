3b56e9144700f7eb339845c48d87ca30953e588d
niceyuanze <niceyuanze@gmail.com>
2020-11-13 17:56:01 +0800

    一步两步代码完善加缓存

--- src/main/java/datastructure/recursion/OneStepOrTwoStap.java
+++ src/main/java/datastructure/recursion/OneStepOrTwoStap.java
@@ -1,6 +1,16 @@
 package datastructure.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OneStepOrTwoStap {
+
+    private static Map<Integer,Integer> cache = new HashMap<>();
+    static {
+        cache.put(1,1);
+        cache.put(2,2);
+    }
+
 
 //    F(n) = F(n-1)+ F(n-2)
 //    F
@@ -19,10 +29,30 @@ public class OneStepOrTwoStap {
         if(n < 3){
             return n;
         }
-        return getStapsInRecursion(n-1) + getStapsInRecursion(n-2);
+
+        int curr;
+        if(cache.get(n) != null){
+            curr = cache.get(n);
+        }else{
+            curr =getStapsInRecursion(n-1) + getStapsInRecursion(n-2);
+            cache.put(n,curr);
+        }
+        return curr;
+    }
+    public static int getStapsNonRecursion(int n){
+        int pre = 2;
+        int prepre = 1;
+        int result = 0;
+        for(int i =3; i <= n; i++){
+            result= pre + prepre;
+            prepre =pre;
+            pre = result;
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         System.out.println(getStapsInRecursion(10));
+        System.out.println(getStapsNonRecursion(10));
     }
 }
