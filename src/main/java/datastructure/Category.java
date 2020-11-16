82ec772775cbe22edc855e37a066fd5050ee7f57
niceyuanze <niceyuanze@gmail.com>
2020-11-16 20:47:49 +0800

    update category

--- src/main/java/datastructure/Category.java
+++ src/main/java/datastructure/Category.java
@@ -4,14 +4,23 @@ import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class Category {
-
+// todo
 //    https://github.com/wangzheng0822/algo/blob/master/java/07_linkedlist/LinkedListAlgo.java
 //    todo ，浏览器前进后退双栈,四则混合预算
 //    阻塞队列，并发安全队列
-//    递归，检查环的存在，两步走一步走，电影院求当前是第几排
+//    递归，检查环的存在
 //    sout 小于栈异常，这个要研究一下
-//    递归缓存求值完善
+//
 //    排序性能比较
 
+//    课后思考我们讲过，特定算法是依赖特定的数据结构的。我们今天讲的几种排序算法，都是基于数组实现的。
+//    如果数据存储在链表中，这三种排序算法还能工作吗？如果能，那相应的时间、空间复杂度又是多少呢？
+//    欢迎留言和我分享，我会第一时间给你反馈。
+//    如何在 O(n) 的时间复杂度内查找一个无序数组中的第 K 大元素？
+//    希尔排序
+
+
+
+//    done
 
 }
