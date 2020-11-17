f6fcec112969476eb9911626a9cb045bf82d8bd7
niceyuanze <niceyuanze@gmail.com>
2020-11-17 11:06:06 +0800

    完善，插入排序还是要再思考一下

--- src/main/java/datastructure/sort/InsertionSort.java
+++ src/main/java/datastructure/sort/InsertionSort.java
@@ -34,6 +34,37 @@ public class InsertionSort {
         }
     }
 
+    /**
+     * 查询插入位置时， 从头至尾搜索
+     * @param data
+     */
+    private static void fromStartToEnd(int[] data) {
+        for (int i=1; i < data.length; i++) {
+            int value = data[i];
+
+            int[] tmp = new int[2];
+            int change = i;
+            for (int j=0; j < i; j++) {
+                if(value >= data[j]) {
+                    continue;
+                }
+
+                int index = j%2;
+                if (change == i) {
+                    tmp[Math.abs(index-1)] = data[j];
+                    change = j;
+                }
+                tmp[index] = data[j+1];
+                if (0 == index) {
+                    data[j+1] = tmp[index+1];
+                } else {
+                    data[j+1] = tmp[index-1];
+                }
+            }
+            data[change] = value;
+        }
+    }
+
     public static void main(String[] args) {
         Integer[] input = new Integer[]{4,5,3,6,4,5,1};
 //        Integer[] input = new Integer[]{5,4};
